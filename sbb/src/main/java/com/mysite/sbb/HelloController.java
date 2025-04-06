268e4e70088bc210921a57d78f48548072efbc99
lshjju <53454978+lshjju@users.noreply.github.com>
2025-04-06 16:10:56 +0900

    Update HelloController.java

--- sbb/src/main/java/com/mysite/sbb/HelloController.java
+++ sbb/src/main/java/com/mysite/sbb/HelloController.java
@@ -13,7 +13,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 // return - 리턴 값 뷰에 출력
 @Controller
 public class HelloController {
+	// http://localhost:8080/hello 유알엥 료청 발생하면 헬로 메서드가 실행됨
+	// /hello - 유알엘과 헬로메서드를 매빙하는 롤
+	// 유알엘과 메서드네임이 동일할 필요는 없음
 	@GetMapping("/hello")
+	// 메서드의 출력 결과가 문자열 그 자체임을 의미
+	// 리턴의 문자열을 리턴
 	@ResponseBody
 	public String hello() {
 		return "Hello Spring Boot Board";
