3470ddc02a67c9f6ed3882af84d6a0332015e8f3
lshjju <53454978+lshjju@users.noreply.github.com>
2025-04-06 15:47:44 +0900

    Update Answer.java

--- sbb/src/main/java/com/mysite/sbb/answer/Answer.java
+++ sbb/src/main/java/com/mysite/sbb/answer/Answer.java
@@ -19,6 +19,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+// 질문에 대한 답 제목과 내용을 담는 롤	
 public class Answer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,8 +29,11 @@ public class Answer {
 	private String content;
 
 	private LocalDateTime createDate;
-
+	
+// 매니투원 - 답은 많이 질문 하나
 	@ManyToOne
+	
+// 앤서의 퀘스천과 열결 롤	
 	private Question question;
 
 	@ManyToOne
