68ba4978427b280d29bf810f19f1832dfc370ecf
lshjju <53454978+lshjju@users.noreply.github.com>
2025-04-06 17:51:38 +0900

    Update Answer.java

--- sbb/src/main/java/com/mysite/sbb/answer/Answer.java
+++ sbb/src/main/java/com/mysite/sbb/answer/Answer.java
@@ -16,10 +16,25 @@ import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
+// @Getter - 게터 자동생성
+// @Setter - 세터 자동생성 - 엔티티는 세터 사용안함 - 여기선 레슨용으로 사용
+// @Entity - 엔티티 선언
+// public - 칠드런
+// @Id - pk 지정
+// @GeneratedValue - 데이터 넘버 알아서 1씩 증가
+// private -
+
+// ? @Column - 텍스트 제한없음
+// private - 
+
+// private - 데이터 생성 시간
+
+// @ManyToOne - 앤서는 많고 퀘스천은 하나 - 페어런츠와 칠드런 연결
+// private - 퀘스천 엔티티 참조
+
 @Getter
 @Setter
 @Entity
-// 질문에 대한 답 제목과 내용을 담는 롤	
 public class Answer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,10 +45,7 @@ public class Answer {
 
 	private LocalDateTime createDate;
 	
-// 매니투원 - 답은 많이 질문 하나
-	@ManyToOne
-	
-// 앤서의 퀘스천과 열결 롤	
+	@ManyToOne	
 	private Question question;
 
 	@ManyToOne
