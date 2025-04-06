38522e05ca76efafd5e0e324a8483ae69e81003e
lshjju <53454978+lshjju@users.noreply.github.com>
2025-04-06 18:03:44 +0900

    Update AnswerRepository.java

--- sbb/src/main/java/com/mysite/sbb/answer/AnswerRepository.java
+++ sbb/src/main/java/com/mysite/sbb/answer/AnswerRepository.java
@@ -2,6 +2,7 @@ package com.mysite.sbb.answer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// public - 엔티티가 세팅한 디비를 씨알유디 할 인터페이스
 public interface AnswerRepository extends JpaRepository<Answer, Integer> {
 
 }
