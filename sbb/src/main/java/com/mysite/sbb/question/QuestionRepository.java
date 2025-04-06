e058f69cfd8bb69eba8f86f21bd76508fa5a3f2c
lshjju <53454978+lshjju@users.noreply.github.com>
2025-04-06 18:02:13 +0900

    Update QuestionRepository.java

--- sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -9,11 +9,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-// 엔티티 - 디비 테이블 생성 롤
-// 리포지토리 - 디비 테이블에 접근하고 시알유디를 가능하게 하는 롤 - 데이터 관리 메서드 제공 - 예 파인드올 세이브 등
-// 제피에이리포지토리 - 제이피에이가 제공하는 인터페이스 - 씨알유디 메서드 내장 - 디비관리가 편리
-// JpaRepository<Question, Integer> - 퀘스천 엔티티로 리포지토리를 생성함 - 인티저는 엔티티 기본키
-// findAll - 콜받으면 페이지어블 클래스를 사용해서 페이지 자료형 타입 오브젝을 리턴
+
+// public - 엔티티가 세팅한 디비를 씨알유디 할 인터페이스 
+// Question -
+// Question -
+// List -
+// Page -
+// Page - findAll - 콜받으면 페이지어블 클래스를 사용해서 페이지 자료형 타입 오브젝을 리턴
+// @Query - 
+// Page - 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 	Question findBySubject(String subject);
 
