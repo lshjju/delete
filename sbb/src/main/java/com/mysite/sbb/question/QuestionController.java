cb038de30ab2c45d33b817a44c5578dbbb75ccf5
lshjju <53454978+lshjju@users.noreply.github.com>
2025-04-07 21:03:52 +0900

    Update QuestionController.java

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -61,8 +61,8 @@ public class QuestionController {
 		return "question_list";
 	}
 	
-// @GetMapping - 퀘스천리스트아이디뷰 콜 받음 
-// public - 모델/퀘스천아이디,변하는 아이디값은 패스배리어블 필요,ㅇ우위윙에엥이있ㄴ느는/앤서폼,question_detail이 거시기와 연결되었으므로 추가	- 
+// @GetMapping - 퀘스천리스트아이디뷰 콜 받음 - 배리어블네임은 아래 파라미터 배리어블네임과 같아야 함
+// public - 모델/퀘스천아이디,변하는 아이디값은 패스배리어블 필요/앤서폼,question_detail이 거시기와 연결되었으므로 추가
 // Question - 아이디 - 위에 밸류와 패스배리어블 파라미터 일치
 // model - 
 // return - 퀘스천디테일뷰 리턴	
