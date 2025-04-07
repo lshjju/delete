735077dd2c3ddd4121c4c00b6b807c203ec32f85
lshjju <53454978+lshjju@users.noreply.github.com>
2025-04-07 22:11:38 +0900

    Update DataNotFoundException.java

--- sbb/src/main/java/com/mysite/sbb/DataNotFoundException.java
+++ sbb/src/main/java/com/mysite/sbb/DataNotFoundException.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 // @ResponseStatus - 익셉션 발생할때 특정 코드 리턴 - 설정된 에이치티티피 상태코드와 이유를 클라이언트에게 리턴
 // public - 디비에서 특정 엔티티 혹은 데이터를 찾을 수 없는 예외 발생시 실행되는 클래스 - 런타임익셉션 상속
 // private - 
-// ? public - 스트링 메시지 출력
-// ? super - 페어런트 메서드 사용
+// public - 스트링 메시지 출력
+// super - 익셉션뷰에 리스판스스태튜스 리즌 메시지 뜸
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "entity not found")
 public class DataNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
