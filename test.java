8ac84b97873b9cf34dbbe17a842c9062dfc2be18
lshjju <53454978+lshjju@users.noreply.github.com>
2025-05-15 21:08:39 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,3 +1,15 @@
+public class QuestionService {
+
+    public void create(String subject, String content) {
+      Question q = new Question();
+      q.setSubject(subject);
+      q.setContent(content);
+      q.setCreateDate(LocalDateTime.now();
+      this.questionRepository.sav(q);
+    }
+}
+
+
 @PostMapping("/create")
   public String questionCreate(@RequestParam(value="subject") String subject, @RequsetParam(value="content") String content){
     return "redirect://question/list";
