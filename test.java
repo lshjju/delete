e4810e591cdaa3f5e24bf016cbd8e67b03086e22
lshjju <53454978+lshjju@users.noreply.github.com>
2025-05-17 20:42:01 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,3 +1,15 @@
+@Getter
+@Setter
+public class QuestionForm {
+    @NotEmpty(message="제목은 필수항목입니다.")
+    @Size(max=200)
+    private String subject;
+
+    @NotEmpty(message="내용은 필수항목입니다.")
+    private String content;
+}
+
+
 public class QuestionService {
 
     public void create(String subject, String content) {
