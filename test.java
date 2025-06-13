b1ea657d27709eea2f920ce3507b5b7d9bb5ba46
lshjju <53454978+lshjju@users.noreply.github.com>
2025-06-13 14:51:14 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,16 +1,12 @@
 (... 생략 ...)
-import java.util.Optional;
-import com.mysite.sbb.DataNotFoundException;
-(... 생략 ...)
-public class QuestionService {
+public class QuestionController {
 
     (... 생략 ...)
 
-    public Question getQuestion(Integer id){
-	Optional<Question> question = this.questionRepository.findByid(id);
-	if(question.isPresesnt() {
-		return question.get();
-	} else {
-		throw new DataNotFoundException("question not found");
-	}
+    @GetMapping(value = "/question/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id){
+	Question question = this.questionService.getQuestion(id);
+	model.addAttribute("question", question);
+	return "question_detail"
+		}
 }
