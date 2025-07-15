733de2d78facec02bfec085d53c702773484de92
lshjju <53454978+lshjju@users.noreply.github.com>
2025-07-15 13:40:39 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,12 +1,14 @@
-(... 생략 ...)
-public class QuestionController {
+package com.mysite.sbb.answer;
 
-    (... 생략 ...)
+import jakarta.validation.constraints.NotEmpty;
 
-    @GetMapping("/create")
-    public String questionCreate(QuestionForm questionForm) {
-        return "question_form";
-    }
+import lombok.Getter;
+import lombok.Setter;
 
-    (... 생략 ...)
+@Getter
+@Setter
+public class AnswerForm {
+    @NotEmpty(message = "내용은 필수항목입니다.")
+    private String content;
+    
 }
