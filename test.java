eb6e80e118525857b4348a5f5aa36f0f0cacbe60
lshjju <53454978+lshjju@users.noreply.github.com>
2025-08-04 13:42:00 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,30 +1,35 @@
 package com.mysite.sbb; 
 
 import java.time.LocalDateTime; 
+import java.util.List; 
 
+import jakarta.persistence.CascadeType; 
 import jakarta.persistence.Column; 
 import jakarta.persistence.Entity; 
 import jakarta.persistence.GeneratedValue; 
 import jakarta.persistence.GenerationType; 
 import jakarta.persistence.Id; 
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany; 
+
 import lombok.Getter; 
 import lombok.Setter; 
 
 @Getter 
 @Setter 
 @Entity 
-public class Answer { 
+public class Question { 
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY) 
-    private Integer id;
+    private Integer id; 
+
+    @Column(length = 200) 
+    private String subject; 
 
     @Column(columnDefinition = "TEXT") 
-    private String content;
+    private String content; 
 
-    private LocalDateTime createDate;
+    private LocalDateTime createDate; 
 
-    @ManyToOne 
-    private Question question;
-    
+    @OneToMany(mappeBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
 }
