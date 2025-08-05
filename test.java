a53bea7c6c2de943d37a0f600642989e1d28c5a1
lshjju <53454978+lshjju@users.noreply.github.com>
2025-08-05 14:43:30 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,35 +1,7 @@
-package com.mysite.sbb; 
+package com.mysite.sbb;
 
-import java.time.LocalDateTime; 
-import java.util.List; 
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import jakarta.persistence.CascadeType; 
-import jakarta.persistence.Column; 
-import jakarta.persistence.Entity; 
-import jakarta.persistence.GeneratedValue; 
-import jakarta.persistence.GenerationType; 
-import jakarta.persistence.Id; 
-import jakarta.persistence.OneToMany; 
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
 
-import lombok.Getter; 
-import lombok.Setter; 
-
-@Getter 
-@Setter 
-@Entity 
-public class Question { 
-    @Id 
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
-    private Integer id; 
-
-    @Column(length = 200) 
-    private String subject; 
-
-    @Column(columnDefinition = "TEXT") 
-    private String content; 
-
-    private LocalDateTime createDate; 
-
-    @OneToMany(mappeBy = "question", cascade = CascadeType.REMOVE)
-    private List<Answer> answerList;
 }
