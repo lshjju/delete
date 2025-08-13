c04113847265f0857bba5c494e82f23c9547968e
lshjju <53454978+lshjju@users.noreply.github.com>
2025-08-13 12:49:02 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,11 +1,21 @@
-(... 생략 ...)
-public class QuestionController {
+package com.mysite.sbb.question;
 
-    (... 생략 ...)
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
-    @PostMapping("/create")
-    public String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
-        this.questionService.create(subject, content);
-        return "redirect:/question/list";
-    }
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QuestionForm {
+    @NotEmpty(message="제목은 필수항목입니다.")
+    @Size(max=200)
+    private String subject;
+
+    @NotEmpty(message="내용은 필수항목입니다.")
+    private String content;
+    
+
+
 }
