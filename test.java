3f6b0058ac44ccc64f252b3994fc55c6b90bfb8d
lshjju <53454978+lshjju@users.noreply.github.com>
2025-08-15 22:43:09 +0900

    Update test.java

--- test.java
+++ test.java
@@ -2,21 +2,17 @@ package com.mysite.sbb.question;
 
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@Controller
-public class QuestionController {
+@Service
+public class QuestionService {
 
-    
     private final QuestionRepository questionRepository;
-    @GetMapping("/question/list")
-    public String list(Model, model) {
-        
-        return "question_list";
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
     }
 }
