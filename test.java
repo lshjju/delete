ea188ed82df00857ad60f478035cdc21340cc32e
lshjju <53454978+lshjju@users.noreply.github.com>
2025-08-16 15:42:34 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,13 +1,21 @@
-package com.mysite.sbb;
+(... 생략 ...)
+import org.springframework.web.bind.annotation.RequestMapping;
+(... 생략 ...)
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+@RequestMapping("/question")
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
 
-@ResponseStatus(value = httpStatus.NOT_FOUND, reasun = "entity not found")
-public class DataNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-    public DataNotFoundException(String message){
-        super(message);
-    
+    private final QuestionService questionService;
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        (... 생략 ...)
+    }
+
+    @GetMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        (... 생략 ...)
     }
 }
