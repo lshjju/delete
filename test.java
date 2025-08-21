895416cb28c30f3dca97cbb0e5cdbe1b32b3176e
lshjju <53454978+lshjju@users.noreply.github.com>
2025-08-21 14:49:38 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,39 +1,29 @@
-package com.mysite.sbb.user;
-
 (... 생략 ...)
-import org.springframework.dao.DataIntegrityViolationException;
-(... 생략 ...)
-public class UserController {
 
-    (... 생략 ...)
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+            .csrf((csrf) -> csrf
+                .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+            .headers((headers) -> headers
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                    XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+            .formLogin((formLogin) -> formLogin
+                       .loginPage("/user/login")
+                       .defaultSuccessUrl("/")
+                
+            )
+        ;
+        return http.build();
+    }
 
-    @PostMapping("/signup")
-    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "signup_form";
-        }
-
-        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
-            bindingResult.rejectValue("password2", "passwordInCorrect", 
-                    "2개의 패스워드가 일치하지 않습니다.");
-            return "signup_form";
-        }
-
-        try{
-            userService.create(userCreateForm.getUsername(), 
-                    userCreateForm.getEmail(), userCreateForm.getPassword1());
-        }catch(DataIntegrityViolationException e){
-            e.printStackTrace();
-            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
-            return "signup_form";
-        }catch(Exception e){
-            e.printStackTrace();
-            bindingResult.reject("signupFailed", e.getMessage())
-                return "signup_form";
-        }
-
-
-
-        return "redirect:/";
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
