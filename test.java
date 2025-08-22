3cec5bec4e0adb757be209fba8a047c9a409eb11
lshjju <53454978+lshjju@users.noreply.github.com>
2025-08-22 11:57:40 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,29 +1,10 @@
 (... 생략 ...)
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig  {
-
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
-            .csrf((csrf) -> csrf
-                .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
-            .headers((headers) -> headers
-                .addHeaderWriter(new XFrameOptionsHeaderWriter(
-                    XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
-            .formLogin((formLogin) -> formLogin
-                .loginPage("/user/login")
-                .defaultSuccessUrl("/"))
-            .logout((logout) -> logout
-                    .logoutRequestMatche(new AntPathRequestMatcher("/user/logout"))
-                    .logoutSuccessUrl("/")
-                    .invalidateHttpSession(true));
-        ;
-        return http.build();
-    }
-
+import jakarta.persistence.ManyToOne;
+import com.mysite.sbb.user.SiteUser;
+(... 생략 ...)
+public class Question {
     (... 생략 ...)
+
+    @ManyToOne
+    private SiteUser author;
 }
