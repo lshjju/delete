1a3488966762e77be5fbef77eab9cf662e64002c
lshjju <53454978+lshjju@users.noreply.github.com>
2025-08-23 13:29:17 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,16 +1,20 @@
 (... 생략 ...)
-import com.mysite.sbb.user.SiteUser;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 (... 생략 ...)
-public class QuestionService {
+public class QuestionController {
 
     (... 생략 ...)
 
-    public void create(String subject, String content, SiteUser user) {
-        Question q = new Question();
-        q.setSubject(subject);
-        q.setContent(content);
-        q.setCreateDate(LocalDateTime.now());
-        q.setAuthor(user);
-        this.questionRepository.save(q);
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String questionModify(QuestionForm questionForm, @PathVariable("id") Integer id, Principal principal) {
+        Question question = this.questionService.getQuestion(id);
+        if(!question.getAutor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        questionForm.setSubject(question.getSubject());
+        questionForm.setContent(question.getContent());
+        return "question_form";
     }
 }
