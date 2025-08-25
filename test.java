bb5cd330b5443f9a93306eba8e245a8fc4ab4d09
lshjju <53454978+lshjju@users.noreply.github.com>
2025-08-25 22:08:50 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,20 +1,32 @@
-(... 생략 ...)
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-(... 생략 ...)
-public class QuestionController {
+package com.mysite.sbb;
 
-    (... 생략 ...)
+import java.time.LocalDateTime;
 
-    @PreAuthorize("isAuthenticated()")
-    @GetMapping("/modify/{id}")
-    public String questionModify(QuestionForm questionForm, @PathVariable("id") Integer id, Principal principal) {
-        Question question = this.questionService.getQuestion(id);
-        if(!question.getAutor().getUsername().equals(principal.getName())){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
-        }
-        questionForm.setSubject(question.getSubject());
-        questionForm.setContent(question.getContent());
-        return "question_form";
-    }
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+    
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    
 }
