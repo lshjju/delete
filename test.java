ebae0b2a76b69f55545237d93bee7f15b524849a
lshjju <53454978+lshjju@users.noreply.github.com>
2025-08-27 11:33:33 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,30 +1,15 @@
-package com.mysite.sbb;
+package com.mysite.sbb.question;
 
-import java.time.LocalDateTime;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+@Controller
+public class QuestionController {
 
-
-@SpringBootTest
-class SbbApplicationTests {
-
-    @Autowired
-    private QuestionRepository questionRepository;
-
-    @Test
-    void testJpa() {        
-      Question q1 = new Question();
-      q1.setSubject("sbb가 무엇인가요?");
-      q1.setContent("sbb에 대해서 알고 싶습니다.");
-      q1.setCreateDate(LocalDateTime.now());
-      this.questionRepository.save(q1);
-
-      Question q2 = new Question();
-      q2.setSubject("스프링부트 모델 질문입니다.");
-      q2.setContent("id는 자동으로 생성되나요?");
-      q2.setCreateDate(LocalDateTime.now());
-      this.questionRepository.save(q2);
+    @GetMapping("/question/list")
+   
+    public String list(){
+        return "question_list";
     }
 }
