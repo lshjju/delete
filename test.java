db9b448470124c54040d699741af0242841c0e82
lshjju <53454978+lshjju@users.noreply.github.com>
2025-08-28 15:13:20 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,12 +1,13 @@
 (... 생략 ...)
-public class QuestionController {
+public class AnswerController {
 
-    (... 생략 ...)
+    private final QuestionService questionService;
+    private final AnswerService answerService;
 
-    @GetMapping(value = "/question/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
         Question question = this.questionService.getQuestion(id);
-        model.addAttribute("question", question);
-        return "question_detail";
+        this.answerService.create(question, content);
+        return String.format("redirect:/question/detail/%s", id);
     }
 }
