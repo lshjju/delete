fcc680b7af6612d8cbce4dc06825ed9e016a0944
lshjju <53454978+lshjju@users.noreply.github.com>
2025-09-04 14:15:45 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,13 +1,10 @@
 (... 생략 ...)
-public class AnswerController {
 
-    private final QuestionService questionService;
-    private final AnswerService answerService;
+public class QuestionController {
 
-    @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
-        Question question = this.questionService.getQuestion(id);
-        this.answerService.create(question, content);
-        return String.format("redirect:/question/detail/%s", id);
-    }
+    (... 생략 ...)
+
+    @GetMapping("/create")
+    public String questionCreate(){
+    return "question_form";
 }
