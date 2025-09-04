e1c02b53fdaaf44d3b24512e2ed3e3b18ced0c46
lshjju <53454978+lshjju@users.noreply.github.com>
2025-09-04 14:20:32 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,10 +1,22 @@
-(... 생략 ...)
+package com.mysite.sbb.question;
 
+(... 생략 ...)
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+(... 생략 ...)
 public class QuestionController {
 
     (... 생략 ...)
 
     @GetMapping("/create")
-    public String questionCreate(){
-    return "question_form";
+    public String questionCreate() {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+        public String questionCreate(@RequestParam(value="subject") String subject, 
+                                     @RequestParam(value="content") String content){
+        return "redirect:/question/list";
+
+    }
 }
