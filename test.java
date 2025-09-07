effbe1d32f62d1d53bdc5e720af787c304eb8aa8
lshjju <53454978+lshjju@users.noreply.github.com>
2025-09-07 14:49:29 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,17 +1,12 @@
 (... 생략 ...)
-import jakarta.validation.Valid;
-import org.springframework.validation.BindingResult;
-(... 생략 ...)
 public class QuestionController {
 
     (... 생략 ...)
 
-    @PostMapping("/create")
-    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "question_form";
-        }
-        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
-        return "redirect:/question/list";
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
     }
+
+    (... 생략 ...)
 }
