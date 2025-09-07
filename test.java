fc1c80fb880dbcb5fae24c7b4f6443a86a0e9d38
lshjju <53454978+lshjju@users.noreply.github.com>
2025-09-07 14:54:36 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,12 +1,20 @@
 (... 생략 ...)
-public class QuestionController {
+import jakarta.validation.Valid;
+import org.springframework.validation.BindingResult;
+(... 생략 ...)
+public class AnswerController {
 
     (... 생략 ...)
 
-    @GetMapping("/create")
-    public String questionCreate(QuestionForm questionForm) {
-        return "question_form";
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, 
+            @Valid AnswerForm answerForm, BindingResult bindingResult) {
+        Question question = this.questionService.getQuestion(id);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        this.answerService.create(question, answerForm.getContent());
+        return String.format("redirect:/question/detail/%s", id);
     }
-
-    (... 생략 ...)
 }
