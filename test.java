9df02b4946008acd8341449ad3529955d85d592e
lshjju <53454978+lshjju@users.noreply.github.com>
2025-10-04 13:20:41 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,9 +1,21 @@
-(... 생략 ...)
+package com.mysite.sbb;
 
-dependencies {
-    (... 생략 ...)
- implementation 'org.springframework.boot:spring-boot-starter-security'
-    implementation 'org.thymeleaf.extras:thymeleaf-extras-springsecurity6'
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+            .authorizeHttpRequests((authorizenHttpRequests) -> authorzenHttpPequests
+                                   .requestMatchers(new AntPathRequestMatcher("/**)).pemitAll());
+        return http.build();
+
+
 }
-
-(... 생략 ...)
