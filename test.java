6e91d6b9f14a52b63a27a91d7123a538fa6372c3
lshjju <53454978+lshjju@users.noreply.github.com>
2025-10-22 15:55:21 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,38 +1,10 @@
-package com.mysite.sbb.user;
-
 (... 생략 ...)
-import org.springframework.dao.DataIntegrityViolationException;
+import jakarta.persistence.ManyToOne;
+import com.mysite.sbb.user.SiteUser;
 (... 생략 ...)
-public class UserController {
-
+public class Question {
     (... 생략 ...)
 
-    @PostMapping("/signup")
-    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "signup_form";
-        }
-
-        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
-            bindingResult.rejectValue("password2", "passwordInCorrect", 
-                    "2개의 패스워드가 일치하지 않습니다.");
-            return "signup_form";
-        }
-
-
-        try{
-            userService.create(userCreateForm.getUsername(), 
-                    userCreateForm.getEmail(), userCreateForm.getPassword1());
-
-        }catch(DataIntegrityViolationException e){
-            e.printStactTrace();
-            bindingResult.reject("signupFailled", "이미 등록된 사용자입니다.");
-            return "singup_form";
-        }catch(Exception e){
-            e.printStackTrace();
-            bindingResult.reject("signupFailed", e.getMessage());
-            return "signup_form";
-        }
-        return "redirect:/";
-    }
+    @ManyToOne
+    private SiteUser author;
 }
