6f549ca8ac786e44e2570bdff774e148bf72b1d7
lshjju <53454978+lshjju@users.noreply.github.com>
2025-11-20 15:41:35 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,13 +1,17 @@
 (... 생략 ...)
-import java.security.Principal;
+import java.util.Optional;
+import com.mysite.sbb.DataNotFoundException;
 (... 생략 ...)
-public class AnswerController {
+@Service
+public class UserService {
 
     (... 생략 ...)
 
-    @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, 
-            @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal) {
-        (... 생략 ...)
+    public SiteUser getUser(String username) {
+        Optional<SiteUser> siteUser = this.userRepository.finByusername(username);
+    if(siteUser.isPresent()){
+        return siteUser.get();
+    }else{
+        throw new DataNotFoundException("siteUser not found");
     }
 }
