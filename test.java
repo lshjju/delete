54456b00ee8d9bb65ffb4e940b5a20882e2590c1
lshjju <53454978+lshjju@users.noreply.github.com>
2025-11-22 13:36:36 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,17 +1,17 @@
 (... 생략 ...)
-import java.util.Optional;
-import com.mysite.sbb.DataNotFoundException;
+import com.mysite.sbb.user.SiteUser;
 (... 생략 ...)
-@Service
-public class UserService {
+public class AnswerService {
 
     (... 생략 ...)
 
-    public SiteUser getUser(String username) {
-        Optional<SiteUser> siteUser = this.userRepository.finByusername(username);
-    if(siteUser.isPresent()){
-        return siteUser.get();
-    }else{
-        throw new DataNotFoundException("siteUser not found");
+    public Answer create(Question question, String content, SiteUser author) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        answer.setAUthor(author);
+        this.answerRepository.save(answer);
+        return answer;
     }
 }
