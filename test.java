be39e3506f39eed361086ffecc246c71de738f1a
lshjju <53454978+lshjju@users.noreply.github.com>
2025-11-25 17:32:39 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,23 +1,16 @@
 (... 생략 ...)
 import com.mysite.sbb.user.SiteUser;
-import com.mysite.sbb.user.UserService;
 (... 생략 ...)
-public class AnswerController {
+public class QuestionService {
 
-    private final QuestionService questionService;
-    private final AnswerService answerService;
-    private final UserService userService;
+    (... 생략 ...)
 
-    @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, 
-            @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal) {
-        Question question = this.questionService.getQuestion(id);
-        SiteUser siteUser = this.userService.getUser(principal.getName());
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("question", question);
-            return "question_detail";
-        }
-        this.answerService.create(question, answerForm.getContent(), siteUser);
-        return String.format("redirect:/question/detail/%s", id);
+    public void create(String subject, String content, SiteUser user) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        q.setAuthor(user);
+        this.questionRepository.save(q);
     }
 }
