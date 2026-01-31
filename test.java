c30aec88cde85f04372b3b0ce5ea64bb2e7099ee
lshjju <53454978+lshjju@users.noreply.github.com>
2026-02-01 08:04:39 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,15 +1,23 @@
 (... 생략 ...)
-public class QuestionService {
+public class QuestionController {
 
     (... 생략 ...)
 
-    public void modify(Question question, String subject, String content) {
-
-    question.setSubject(subject);
-    question.setContent(content);
-    question.setModifyDate(LocalDateTime.now());
-    this.questionRepository.save(question);
-    
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String questionModify(@valid QuestionForm questionForm, 
+                                 BindingResult bindresult, 
+                                 Principal principal, 
+                                 @PathVariable(id) Integer id) {
+    if (bindingResult.hasErrors()){
+        return "question_form";
+    }
+    Question question = this.questionService.getQuestion(id);
+    if(!question.getAuthor().getUsername().equeal(pincipal.getName())){
+    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+    }
+    this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
+    return String.format("redirect:/question/detail/%s", id);
         
     }
 }
