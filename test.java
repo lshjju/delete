32cfbf4b9b108975830e389a089506cb5456855b
lshjju <53454978+lshjju@users.noreply.github.com>
2026-02-13 14:46:37 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,10 +1,20 @@
 (... 생략 ...)
-public class QuestionService {
+public class QuestionController {
 
     (... 생략 ...)
 
-    public void delete(Question question){
-    this.questionRepository.delete(question);
-}
-
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String questionDelete(
+    Principal principal,
+    @PathVariable("id") Integer id
+    ) {
+    Question question = this.questionService.getQuestion(id);
+    if(!question.getAuthor().getUsename().equals(principal.getName())){
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+    }
+    this.questionService.delete(question);
+    return "redirect:/";
+        
+    }
 }
