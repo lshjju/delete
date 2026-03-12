d23882f6537ba4a734b33a2f23cc834caf63d34f
lshjju <53454978+lshjju@users.noreply.github.com>
2026-03-12 14:30:32 +0900

    Update test.java

--- test.java
+++ test.java
@@ -11,9 +11,17 @@ public class AnswerController {
     @GetMapping("/modify/{id}")
     public String answerModify(AnswerForm answerForm, @PathVariable("id") Inger id, Principal principal) {
     Answer answer = this.answerService.getAnswer(id);
-    if(){
+    if(bindingResult.hasErrors()){
+        return "answer_form"
+    }
+            Answer answer = this.answerService.getAnswer(id);
+
+    if(!answer.getAuthor().getUsername().equals(principal.getName())){
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
         
     }
+    this.answerService.modify(answer, answerForm.getConent());
+    return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
         
     }
 }
