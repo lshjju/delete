6485d6fed3004883dd3bc6990cbf1983778810f1
lshjju <53454978+lshjju@users.noreply.github.com>
2026-03-20 14:54:59 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,27 +1,21 @@
 (... 생략 ...)
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.server.ResponseStatusException;
-(... 생략 ...)
 public class AnswerController {
 
     (... 생략 ...)
 
     @PreAuthorize("isAuthenticated()")
-    @GetMapping("/modify/{id}")
-    public String answerModify(AnswerForm answerForm, @PathVariable("id") Inger id, Principal principal) {
-    Answer answer = this.answerService.getAnswer(id);
+    @PostMapping("/modify/{id}")
+    public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult, @PathVariable("id") Integer id, Principal principal) {
     if(bindingResult.hasErrors()){
-        return "answer_form"
+        return "answer_form";
     }
-            Answer answer = this.answerService.getAnswer(id);
-
-    if(!answer.getAuthor().getUsername().equals(principal.getName())){
+    Answer answer = this.answerService.getAnswer(id);
+    if(!answer.getAnswer().getUsername().equals(principal.getName())){
         throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
-        
     }
-    this.answerService.modify(answer, answerForm.getConent());
+    this.answerService.modify(answer, answerForm.getContent());
     return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        
         
     }
 }
